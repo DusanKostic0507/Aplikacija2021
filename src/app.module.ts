c7f1365faf24c6d80564666959de7bea244744d6
Dusan Kostic <dusan.kostic0507@gmail.com>
2021-05-24 18:46:28 +0200

    Create the administrator table service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AdministratorService],
 })
 export class AppModule {}
