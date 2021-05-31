a4531b66b42fe7f920f8d45d59665052d78c7e74
Dusan Kostic <dusan.kostic0507@gmail.com>
2021-05-31 22:05:06 +0200

    Implement upload error responsesthrough the API

--- src/misc/api.response.class.ts
+++ src/misc/api.response.class.ts
@@ -3,7 +3,7 @@ export class ApiResponse {
     statusCode: number;
     message: string | null;
 
-    constructor(status: string, statusCode: number, message: | null = null) {
+    constructor(status: string, statusCode: number, message: string | null = null) {
         this.status = status;
         this.statusCode = statusCode;
         this.message = this.message;
