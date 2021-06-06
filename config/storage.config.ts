40eafaae4387fa3323d4c629391e3227e644b1cf
Dusan Kostic <dusan.kostic0507@gmail.com>
2021-06-06 20:47:06 +0200

    Do minor code fixes and optimizations

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,6 +1,18 @@
 export const StorageConfig = {
-    photoDestinacion: '../storage/photos/',
-    photoMaxFileSize: 1024 * 1024 * 3,
-    photoThumbSize: { width: 120, height: 100 },
-    photoSmallSize: { width: 320, height: 240 },
+    photo: {
+        destinacion: '../storage/photos/',
+        maxSize: 3 * 1024 * 1024,
+        resize: {
+            thumb: {
+                width: 120,
+                height: 100,
+                directory: 'thumb/'
+            },
+            small: {
+                width: 320,
+                height: 240,
+                directory: 'small/'
+            }
+        }
+    }
 };
